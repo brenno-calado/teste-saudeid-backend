858fcd33eea34d35df584079f1ccabdf43a2639b
brenno-calado <brenno.calado@gmail.com>
2021-11-24 15:21:49 -0300

    add sample test constants, write expected messages at post.test.js

--- tests/posts.test.js
+++ tests/posts.test.js
@@ -7,6 +7,15 @@ const categoriesSeed = require('../sample/categoriesSeed');
 const postsSeed = require('../sample/postSeed');
 const usersSeed = require('../sample/usersSeed');
 
+const title = 'Is Thy Node as Thou thinks?';
+const description = 'To Node or not to Node? That is the request';
+const author = 'Evelina Escobar Cedraz';
+const categories = ['health', 'technology'];
+
+const unknownAuthor = 'Michelangelo Brasiliensis';
+const unknownCategory = 'cats';
+const wrongFormat = 42;
+
 const mongoDbUrl = `mongodb://localhost:27017/${process.env.DBNAME}`;
 const url = `http://localhost:${process.env.PORT}`;
 
@@ -38,146 +47,142 @@ describe('POST post', () => {
 
   test('when theres no title', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      description,
+      author,
+      categories,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: missing title');
     })
   });
   test('when theres no description', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      author,
+      categories,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: missing description');
     })
   });
   test('when theres no author', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description,
+      categories,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: missing author');
     })
   });
   test('when theres no categories', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description,
+      author,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: missing categories');
     })
   });
   test('when title is not a string', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title: wrongFormat,
+      description,
+      author,
+      categories,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: title is not a string');
     })
   });
   test('when description is not a string', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description: wrongFormat,
+      author,
+      categories,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: description is not a string');
     })
   });
   test('when author is not a string', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description,
+      author: wrongFormat,
+      categories,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: author is not a string');
     })
   });
   test('when categories is not an array of strings', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description,
+      author,
+      categories: wrongFormat,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request: categories is not an array of strings');
     })
   });
   test('when author does not exist', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description,
+      author: unknownAuthor,
+      categories,
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request, try again');
     })
   });
   test('when one of the categories does not exist', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description,
+      author,
+      categories: [...categories, unknownCategory],
     })
     .expect('status', 400)
     .then((response) => {
       const { json } = response;
-      expect(json.message).toBe('');
+      expect(json.message).toBe('Invalid request, try again');
     })
   });
   test('when creating a post is successful', async () => {
     await frisby.post(`${url}/post`, {
-      title: '',
-      description: '',
-      author: '',
-      categories: [],
+      title,
+      description,
+      author,
+      categories,
     })
     .expect('status', 201)
     .then((response) => {
       const { json } = response;
       expect(json.data).toHaveProperty('id');
-      expect(json.data.author).toBe('');
+      expect(json.data.author).toBe(author);
     })
   });
 });
