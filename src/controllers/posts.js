f6becf25e19463c9878a1c891a981f86ae10f5dd
brenno-calado <brenno.calado@gmail.com>
2021-11-24 19:09:16 -0300

    handle post schema and its errors

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,11 +1,14 @@
 // const service = require('../services/posts');
 const { checkPost } = require('./schemas/posts');
+const service = require('../services/posts');
 
 const insert = async (req, res) => {
   const { error } = checkPost(req.body);
-  if (error) {
-    throw error;
-  }
+  if (error) throw error;
+
+  const [result, err] = service.insert(req.body);
+  console.log(result);
+  if (err) throw err;
 
   return res.status(201).json({});
 };
