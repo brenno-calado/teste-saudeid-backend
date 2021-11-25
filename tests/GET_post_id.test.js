878db2ae88781bd8b2fe261754fcc9a9a5181bed
brenno-calado <brenno.calado@gmail.com>
2021-11-25 19:24:32 -0300

    add GET POST_id tests

--- tests/GET_post_id.test.js
+++ tests/GET_post_id.test.js
@@ -11,6 +11,10 @@ const categoriesSeed = require('../sample/categoriesSeed');
 const postsSeed = require('../sample/postSeed');
 const usersSeed = require('../sample/usersSeed');
 
+const {
+  title, description, author, categories,
+} = require('./mocks/post.mock');
+
 const mongoDbUrl = `${MONGO_HOST}${MONGO_PORT}/${DB_NAME}${NODE_ENV}`;
 const url = `${HOST}${PORT}`;
 
@@ -25,18 +29,48 @@ describe('GET /post/:id', () => {
     });
     db = connection.db(`${process.env.DB_NAME}${process.env.NODE_ENV}`);
 
+    await db.collection('users').deleteMany({});
+    await db.collection('users').insertMany(usersSeed);
     await db.collection('categories').deleteMany({});
     await db.collection('categories').insertMany(categoriesSeed);
-  });
-
-  beforeEach(async () => {
-    await db.collection('users').deleteMany({});
     await db.collection('posts').deleteMany({});
-    await db.collection('users').insertMany(usersSeed);
     await db.collection('posts').insertMany(postsSeed);
   });
 
   afterAll(async () => {
     await connection.close();
+  });
+
+  test('1 - when a post exists', async () => {
+    let result;
+
+    await frisby.post(`${url}/post`, {
+      title,
+      description,
+      author,
+      categories,
+    })
+      .expect('status', 201)
+      .then((response) => {
+        const { body } = response;
+        result = JSON.parse(body);
+      });
+
+    await frisby.get(`${url}/post/${result.id}`)
+      .expect('status', 200)
+      .then((response) => {
+        const { body } = response;
+        const parsed = JSON.parse(body);
+        expect(parsed).toHaveProperty('data');
+        expect(typeof parsed.data).toBe('object');
+        expect(parsed.data.id).toBe(result.id);
+        expect(parsed.data.title).toBe(title);
+        expect(parsed.data.description).toContain(description);
+        expect(parsed.data.author).toBe(author);
+        expect(Array.isArray(parsed.data.categories)).toEqual(categories);
+      });
+  });
+  test('2 - when a post does not exist', async () => {
+
   });
 });
